e9f34d067944b4126074880b391969a46d858e22
sariamancharla <93614345+sariamancharla@users.noreply.github.com>
2022-12-19 22:30:05 +0000

    style,onclick,alert msg,variable,singleparent

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 function App() {
+  const name="Star"
+
   return (
-    <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-    </hgroup>
+    <div className="container">
+      <article>
+        <hgroup>
+          <img src="images/group.svg" alt="Hi" />
+          <h1>Meet the <i style={{color:"black",fontSize:"100px"}}>{name}</i></h1>
+          <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+          <button onClick={()=>alert("Hi")}>Submit</button>
+        </hgroup>
+      </article>
+    </div>
   )
 }
 export default App
